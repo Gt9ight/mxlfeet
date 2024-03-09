1d4f4627c7046263bc7291714baa1c4c1c9df5b8
Mariano Sanchez <marianosanchez0307@gmail.com>
2024-03-09 13:35:15 -0500

    small update

--- src/components/home/Search.js
+++ src/components/home/Search.js
@@ -4,7 +4,7 @@ const Search = () => {
   return (
     <div className='search'>
       <div className='searchForm'>
-        <input type='text' />
+        <input type='text' placeholder='search for a fleet'/>
       </div>
       <div className='userChat'>
         <img src='https://d1jyxxz9imt9yb.cloudfront.net/medialib/4059/image/s768x1300/AdobeStock_362156363_441361_reduced.jpg' />
