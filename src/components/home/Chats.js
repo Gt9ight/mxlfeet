10e2b522c5cc62db8f14943022724f6eab97090d
Mariano Sanchez Hernandez <marianosanchez0307@gmail.com>
2024-03-18 07:16:47 -0400

    worked on other components

--- src/components/home/Chats.js
+++ src/components/home/Chats.js
@@ -2,7 +2,31 @@ import React from 'react'
 
 const Chats = () => {
   return (
-    <div>Chats</div>
+    <div className='chats'>
+      <div className='userChat'>
+        <img src='https://d1jyxxz9imt9yb.cloudfront.net/medialib/4059/image/s768x1300/AdobeStock_362156363_441361_reduced.jpg' />
+        <div className='userChatInfo'>
+          <span>plm</span>
+          <p>Hey the fleet is done</p>
+        </div>
+      </div>
+
+      <div className='userChat'>
+        <img src='https://d1jyxxz9imt9yb.cloudfront.net/medialib/4059/image/s768x1300/AdobeStock_362156363_441361_reduced.jpg' />
+        <div className='userChatInfo'>
+          <span>plm</span>
+          <p>Hey the fleet is done</p>
+        </div>
+      </div>
+
+      <div className='userChat'>
+        <img src='https://d1jyxxz9imt9yb.cloudfront.net/medialib/4059/image/s768x1300/AdobeStock_362156363_441361_reduced.jpg' />
+        <div className='userChatInfo'>
+          <span>plm</span>
+          <p>Hey the fleet is done</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/home/Input.js
+++ src/components/home/Input.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import IMG from '../images/img.png'
 
 const Input = () => {
   return (
-    <div>Input</div>
+    <div className='input'>
+      <input type='text' placeholder='type a fleet up' />
+      <div className='send'>
+        <input type='file' style={{display:'none'}} id='file' />
+        <label htmlFor='file'>
+          <img src={IMG}/>
+        </label>
+        <button>send</button>
+      </div>
+    </div>
   )
 }
 
