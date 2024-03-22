5b489f5abf7d26817b123c74c330fd9082862dd7
Mariano Sanchez Hernandez <marianosanchez0307@gmail.com>
2024-03-21 23:05:13 -0400

    adding firebase auth

--- src/components/home/Message.js
+++ src/components/home/Message.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 function Message() {
   return (
-    <div>Message</div>
+    <div className='message owner'>
+      <div className='messageInfo'>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI-ycnCXMU_IhRLsRqnkOnGExWNNDFFy07Kg&usqp=CAU' alt=''/>
+        <span>just now</span>
+      </div>
+      <div className='messageContent'>
+        <p>hello</p>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTa_MutQ_pps4L9z9ca0TlJAsAlqh81a_bvbA&usqp=CAU'/>
+      </div>
+
+    </div>
   )
 }
 
---
+++ src/components/utilis/Firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import {getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword} from 'firebase/auth'
+import {getStorage} from 'firebase/storage'
+import { getFirestore, doc, getDoc, setDoc, collection, writeBatch, query, getDocs} from 'firebase/firestore'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBG6G9d6yRaLSCLnqlRhXDe2FDHU1XBr3Q",
+    authDomain: "ace-fleeting.firebaseapp.com",
+    projectId: "ace-fleeting",
+    storageBucket: "ace-fleeting.appspot.com",
+    messagingSenderId: "205403427962",
+    appId: "1:205403427962:web:a1412b008668298fcb97c3"
+  };
+  
+
+  const app = initializeApp(firebaseConfig);
+  export const db = getFirestore();
