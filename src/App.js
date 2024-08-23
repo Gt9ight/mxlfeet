48faf095c48d231716725f08d9ad3c4231a13d0f
Mariano Sanchez Hernandez <marianosanchez0307@gmail.com>
2024-08-23 02:46:09 -0400

    add

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/login/Login';
 import ThankYou from './components/login/Thankyou';
 
 function App() {
   return (
-    <div>
     <BrowserRouter basename="/mxlfleet">
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/thank-you" element={<ThankYou />} />
+        <Route path="*" element={<Navigate to="/" />} /> {/* Redirects unknown routes */}
       </Routes>
-      </BrowserRouter>
-    </div>
+    </BrowserRouter>
   );
 }
 
