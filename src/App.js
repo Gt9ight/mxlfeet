22c65145892f880e9f63372c10c8677a72d75ff7
Mariano Sanchez Hernandez <marianosanchez0307@gmail.com>
2024-08-23 02:35:18 -0400

    atttempttwo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from './components/login/Login';
 import ThankYou from './components/login/Thankyou';
 
 function App() {
   return (
-    <Router>
+    <div>
+    <BrowserRouter basename="/mxlfleet">
       <Routes>
-        <Route path="/mxlfleet" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/thank-you" element={<ThankYou />} />
       </Routes>
-    </Router>
+      </BrowserRouter>
+    </div>
   );
 }
 
